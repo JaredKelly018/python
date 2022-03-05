fc5a76b74c4e5b3842bcbb5b2fa7890c0b79e2df
Jared Kelly <jpkell046@gmail.com>
2022-03-04 16:24:09 -0800

    basic py
    learning py is so much better than js

---
+++ app.py
@@ -0,0 +1,25 @@
+from pydoc import doc
+import select
+
+
+gender = input('What prouns would you like me to use for you?')
+print('You have chosen ' + gender + '.')
+
+birth_year = input('Enter your birth year:')
+age = 2022 - int(birth_year)
+print(age)
+
+x = input('Please select your first  number')
+y = input('Please select your second number')
+sum = float(x) + float(y)
+print('The sum is `' +str(sum))
+
+upper_case = 'Make Me all Upper case please or Lowe 9'
+print(upper_case.upper())
+print(upper_case.lower())
+print(upper_case.find("or")) #tells you index location
+print('all' in upper_case) #checks to see if str is there
+
+
+Cris = input('Is Cristobal a definable object')
+print(Cris + " definable.")
