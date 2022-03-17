a9132369866f252693e73bb6add36b9d14dfbc2e
Jared Kelly <jpkell046@gmail.com>
2022-03-17 10:03:08 -0700

    Update heightCinvert.py

--- random projects/heightCinvert.py
+++ random projects/heightCinvert.py
@@ -1,13 +1,12 @@
-import sys
 
+import sys
+height = input(("what is your height? "))
+unit = input ("What would you like to conver to in or cm? ").lower()
 try:
-    height = input(("what is your height? "))
+    if unit == "cm":
+        print (f'Your height in cm is {(float(height) * 2.54)}')
+    else:
+        print (f'Your heigh in in is {(float(height) / 2.54)}')
 except ValueError:
     print('Please enter a valid interger.')
-
-unit = input ("What would you like to conver to in or cm? ").lower()
-if unit == "cm":
-    print (f'Your height in cm is {(float(height) * 2.54)}')
-else:
-    print (f'Your heigh in in is {(float(height) / 2.54)}')
 
